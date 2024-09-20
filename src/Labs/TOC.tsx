b95054d69107f5378fb068bb92547879028b9be2
YilinLong <screamylong@gmail.com>
2024-09-20 16:06:41 -0400

    update the link to github repository

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -8,6 +8,7 @@ export default function TOC() {
             <li><Link to="/Labs/Lab2">Lab 2</Link></li>
             <li><Link to="/Labs/Lab3">Lab 3</Link></li>
             <li><Link to="/Kanbas">Kanbas</Link></li>
+            <a href="https://github.com/YilinLong/kanbas-react-web-app/tree/main" id="wd-github" target="_blank">My Github Repository</a><br/>
         </ul>
     );
 }
